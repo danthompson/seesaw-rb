8ee55b30b5dceed4b125b64803281b4b309901f4
Sam Soffes <sam@soff.es>
2013-01-30 18:58:51 -0500

    Handle invitations when voting

--- lib/seesaw/client/votes.rb
+++ lib/seesaw/client/votes.rb
@@ -7,11 +7,18 @@ module Seesaw
       # @param decision_user_id [Fixnum] the decision's owner's ID
       # @param decision_id [Fixnum] the decision's ID
       # @param choice_id [Fixnum] the choice's ID
-      # @param unique_id [Fixnum] optional unique ID. This is required if there is no user.
-      def create_vote(decision_user_id, decision_id, choice_id, unique_id = nil)
+      # @param options [Hash] specifiy `:unique_id` or `:invitation_slug`
+      def create_vote(decision_user_id, decision_id, choice_id, options = {})
+        unique_id = options[:unique_id]
         raise UniqueIDRequired unless unique_id or self.authenticated?
+
         path = "users/#{decision_user_id}/decisions/#{decision_id}/votes"
-        post path, { choice_id: choice_id, unique_id: unique_id }
+        params = { choice_id: choice_id, unique_id: unique_id }
+
+        slug = options[:invitation_slug]
+        params[:invitation] = { slug: slug } if slug and slug[0] == 'i'
+
+        post path, params
       end
     end
   end
--- test/seesaw/client/votes_test.rb
+++ test/seesaw/client/votes_test.rb
@@ -3,12 +3,12 @@ require 'test_helper'
 class VotesTest < Seesaw::TestCase
   def test_create_vote
     VCR.use_cassette 'votes/create' do
-      vote = Seesaw.create_vote(5, 3276, 5381, '021a997d8aa785abed0a25c47b9a07be')
+      vote = Seesaw.create_vote(5, 3276, 5381, unique_id: '021a997')
       assert_equal 5381, vote.choice_id
 
       # Prevent duplicates
       assert_raises Seesaw::BadRequest do
-        Seesaw.create_vote(5, 3276, 5381, '021a997d8aa785abed0a25c47b9a07be')
+        Seesaw.create_vote(5, 3276, 5381, unique_id: '021a997')
       end
     end
   end
