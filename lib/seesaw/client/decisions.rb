bfea7a0788c7f7546fb595fb7b78bb9fb5cd53c8
Aaron Gotwalt <gotwalt@gmail.com>
2013-01-30 09:32:23 -0800

    More decision methods

--- lib/seesaw/client/decisions.rb
+++ lib/seesaw/client/decisions.rb
@@ -4,6 +4,31 @@ module Seesaw
       def decision(user_id, decision_id)
         get "users/#{user_id}/decisions/#{decision_id}"
       end
+
+      def create_decision(user_id)
+        post "users/#{user_id}/decisions"
+      end
+
+      def publish_decision(user_id, decision_id)
+        post "users/#{user_id}/decisions/#{decision_id}/publish"
+      end
+
+      def destroy_decision(user_id, decision_id)
+        boolean_from_response :delete, "users/#{user_id}/decisions/#{decision_id}"
+      end
+
+      def decision_analytics(user_id, decision_id)
+        get "users/#{user_id}/decisions/#{decision_id}/analytics"
+      end
+
+      def flag_decision(user_id, decision_id)
+        boolean_from_response :post, "users/#{user_id}/decisions/#{decision_id}/flag"
+      end
+
+      def unflag_decision(user_id, decision_id)
+        boolean_from_response :post, "users/#{user_id}/decisions/#{decision_id}/unflag"
+      end
+
     end
   end
 end
--- test/seesaw/client/decisions_test.rb
+++ test/seesaw/client/decisions_test.rb
@@ -8,4 +8,48 @@ class DecisionsTest < Seesaw::TestCase
       assert_equal 'Which lamp for the new apartment?', decision.question
     end
   end
+
+  def test_create_decision
+    VCR.use_cassette 'decisions/create' do
+      client = local_client
+      decision = client.create_decision(1)
+      assert_equal 1, decision.id
+      assert_equal 1, decision.user_id
+      assert decision.timeline_key
+    end
+  end
+
+  def test_destroy_decision
+    VCR.use_cassette 'decisions/destroy' do
+      client = local_client
+      decision = client.create_decision(1)
+      assert client.destroy_decision(decision.user_id, decision.id)
+    end
+  end
+
+  def test_decision_analytics
+    VCR.use_cassette 'decisions/analytics' do
+      client = local_client
+      decision = client.create_decision(1)
+      analytics = client.decision_analytics(decision.user_id, decision.id)
+      assert_equal 0, analytics.total_votes
+    end
+  end
+
+  def test_flag_decision
+    VCR.use_cassette 'decisions/flag' do
+      client = local_client
+      decision = client.create_decision(1)
+      assert client.flag_decision(decision.user_id, decision.id)
+    end
+  end
+
+  def test_unflag_decision
+    VCR.use_cassette 'decisions/unflag' do
+      client = local_client
+      decision = client.create_decision(1)
+      assert client.flag_decision(decision.user_id, decision.id)
+      assert client.unflag_decision(decision.user_id, decision.id)
+    end
+  end
 end
